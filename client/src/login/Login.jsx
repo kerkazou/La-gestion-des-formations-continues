94f6c609fce6249bf5cc56b1a9122704f69ab274
kerkazou <kerkazou.zakaria@gmail.com>
2023-01-04 14:53:41 +0100

    fair le front-end du login

--- client/src/login/Login.jsx
+++ client/src/login/Login.jsx
@@ -2,21 +2,37 @@ import './style.css'
 import Input from '../components/Input'
 import Label from '../components/Label'
 import Button from '../components/Button'
+import { useState } from 'react'
 
 export default function Login() {
+
+    const [inputs, setInputs] = useState([
+        { type: 'text', id: 'floatingInput', name: 'email', placeholder: 'Email', label: 'Email address' },
+        { type: 'password', id: 'floatingPassword', name: 'password', placeholder: '••••••••', label: '••••••••' }
+    ])
+
+    const [login, setLogin] = useState({ email: '', password: '' })
+    const onChange = (e) => {
+        const value = e.target.value
+        setLogin({ ...login, [e.target.name]: value });
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(login)
+    }
+
     return (
         <div className="d-flex justify-content-center align-items-center" id='login'>
-            <form className="fw-bold col-lg-4 col-md-6 col-sm-10 col-11 d-flex flex-column justify-content-center align-items-center gap-2 py-5">
+            <form onSubmit={onSubmit} className="fw-bold col-lg-4 col-md-6 col-sm-10 col-11 d-flex flex-column justify-content-center align-items-center gap-2 py-5">
                 <h3 className='fw-bold'>Login</h3>
-                <div className="text-muted h6">Enter email and password to login</div>
-                <div className="form-floating text-muted col-10">
-                    <Input type="text" name='email' id="floatingInput" placeholder="Email" />
-                    <Label htmlFor="floatingInput" label="Email address" />
-                </div>
-                <div className="form-floating text-muted col-10 mt-2">
-                    <Input type="password" name='password' id="floatingPassword" placeholder="Password" />
-                    <Label htmlFor="floatingPassword" label="Password" />
-                </div>
+                <div className="text-muted h6">Enter your email and password to login</div>
+                {inputs.map((input, i) => (
+                    <div className="form-floating text-muted col-10" key={i}>
+                        <Input type={input.type} onChange={onChange} name={input.name} id={input.id} placeholder={input.placeholder} />
+                        <Label htmlFor={input.id} label={input.label} />
+                    </div>
+                ))}
                 <div className="w-50 form-group d-flex justify-content-center position-relative">
                     <Button button='Login' />
                 </div>
