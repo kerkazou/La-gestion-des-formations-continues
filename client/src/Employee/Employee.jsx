983f278f8378c834c03de0d8fb8f2d7b4f375830
kerkazou <kerkazou.zakaria@gmail.com>
2023-01-07 16:13:05 +0100

    Front-end du page Organisme

--- client/src/Employee/Employee.jsx
+++ client/src/Employee/Employee.jsx
@@ -20,7 +20,9 @@ export default function Employee() {
                     <div className="container-fluid pb-3">
                         <div className="d-flex justify-content-between align-items-center px-3 py-2">
                             <span className="fs-4 fw-bold">Employee</span>
-                            <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop" className="fs-3 fw-bold border-0 bg-body"><i class="bi bi-person-plus-fill"></i></button>
+                            <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop" className="fs-3 fw-bold border-0 bg-body">
+                                <i class="bi bi-plus-circle-dotted"></i>
+                            </button>
                         </div>
                         <table className="table table-sm table-responsive text-center">
                             <thead className="fs-5">
--- client/src/Organisme/Organisme.jsx
+++ client/src/Organisme/Organisme.jsx
@@ -19,24 +19,29 @@ export default function Employee() {
                     <Nav />
                     <div className="container-fluid pb-3">
                         <div className="d-flex justify-content-between align-items-center px-3 py-2">
-                            <span className="fs-4 fw-bold">Employee</span>
-                            <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop" className="fs-3 fw-bold border-0 bg-body"><i class="bi bi-person-plus-fill"></i></button>
+                            <span className="fs-4 fw-bold">Organisme</span>
+                            <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop" className="fs-3 fw-bold border-0 bg-body">
+                                <i class="bi bi-plus-circle-dotted"></i>
+                            </button>
                         </div>
                         <table className="table table-sm table-responsive text-center">
                             <thead className="fs-5">
                                 <tr>
-                                    <th className="col-3">Username</th>
-                                    <th className="col-3">Email</th>
-                                    <th className="col-2">Organisme</th>
+                                    <th className="col-3">Numero</th>
+                                    <th className="col-3">Name</th>
                                     <th className="col-2"></th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr className="item">
-                                    <td className="col-3">Username</td>
-                                    <td className="col-3">Email</td>
-                                    <td className="col-2">organisme</td>
-                                    <td className="col-2"></td>
+                                    <td className="col-3">numero</td>
+                                    <td className="col-3">name</td>
+                                    <td className="col-2">
+                                        <div className='d-flex gap-3'>
+                                            <i class="bi bi-pen"></i>
+                                            <i class="bi bi-trash3"></i>
+                                        </div>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
